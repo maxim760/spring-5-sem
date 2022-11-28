be1d0b00d5f098fdf255ae6fbc3d32b8d0b4c361
Maksim <maks.tomanov@mail.ru>
2022-11-28 15:10:49 +0300

    add trigers

--- sql/init.sql
+++ sql/init.sql
@@ -162,7 +162,55 @@ CREATE TABLE `order_goods` (
                                `goods_id` binary(16) NOT NULL,
                                `id` mediumint(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- MySQL dump 10.13  Distrib 8.0.30, for macos12 (arm64)
+--
+-- Host: localhost    Database: pizza_project
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `order_goods`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-28 14:54:15
 --
 -- Дамп данных таблицы `order_goods`
 --
@@ -603,3 +651,46 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+-- triggers
+
+DELIMITER ;;
+CREATE TRIGGER order_goods_AFTER_INSERT
+    AFTER INSERT
+    ON `order_goods` FOR EACH ROW
+BEGIN
+    update products
+        inner join product_goods
+    on products.id = product_goods.product_id
+        inner join goods
+        on goods.id = product_goods.goods_id
+        set count = count - 1
+    where goods.id = new.goods_id;
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE TRIGGER goods_BEFORE_INSERT
+    BEFORE INSERT
+    ON `goods` FOR EACH ROW
+BEGIN
+	set new.discount=10;
+    set new.current_price=new.price*0.9;
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE TRIGGER users_AFTER_DELETE
+    AFTER DELETE
+    ON `users` FOR EACH ROW
+BEGIN
+	delete from addresses where user_id = old.id;
+    delete from user_roles where user_roles.user_id = old.id;
+    delete from orders where orders.user_id = old.id;
+    delete order_goods from order_goods
+    left join orders
+    on order_goods.order_id = orders.id
+    where orders.user_id = old.id;
+END;;
+DELIMITER ;
